9e025b657372562211bae82b21b1438c1f45d0d0
arminv <arminv@users.noreply.github.com>
2021-03-03 21:43:59 -0500

    Progress (mark 3:02:20)

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,32 @@
-export type Dict = {};
+export type Dict<T> = {
+    [k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+// NOTE: `S` is created to represent the 'transformed' values we get at the end when our callback gets applied to each key/value pair:
+export function mapDict<T, S>(dict: Dict<T>, fn: (arg: T, idx: number) => S): Dict<S> {
+    const out: Dict<S> = {};
+    Object.keys(dict).forEach((dKey, idx) => {
+        const thisItem = dict[dKey];
+        if (typeof thisItem !== 'undefined') {
+            out[dKey] = fn(thisItem, idx);
+        }
+    })
+    return out;
+}
+
+// NOTE: we can now use this function, and it returns (via using type parameter `S`) whatever we actually specify without any errors:
+// NOTE: hover over `mapDict`, you will see it tells us we are returning an array of strings:
+mapDict({
+    a: 'a',
+    b: 'b',
+}, (str) => [str])
+
+// NOTE: hover over `mapDict`, you will see it tells us we are returning a dictionary of wrapped values:
+mapDict({
+    a: 'a',
+    b: 'b',
+}, (str) => ({ val: str }))
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T>(dict: Dict<T>) { }
