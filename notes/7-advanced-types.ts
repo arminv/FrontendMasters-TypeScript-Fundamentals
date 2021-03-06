351de9778c23b56c30b2b71452a888ffa84c9fbc
arminv <arminv@users.noreply.github.com>
2021-03-06 18:32:52 -0500

    Progress (mark 3:32:49)

--- notes/7-advanced-types.ts
+++ notes/7-advanced-types.ts
@@ -4,48 +4,50 @@ import { HasEmail, HasPhoneNumber } from "./1-basics";
  * (1) MAPPED TYPES allow the use of an interface to transform keys into values
  */
 
-// interface CommunicationMethods {
-//   email: HasEmail;
-//   phone: HasPhoneNumber;
-//   fax: { fax: number };
-// }
+interface CommunicationMethods {
+    email: HasEmail;
+    phone: HasPhoneNumber;
+    fax: { fax: number };
+}
 
-// function contact<K extends keyof CommunicationMethods>(
-//   method: K,
-//   contact: CommunicationMethods[K] // 💡turning key into value -- a *mapped type*
-// ) {
-//   //...
-// }
-// contact("email", { name: "foo", email: "mike@example.com" });
-// contact("phone", { name: "foo", phone: 3213332222 });
-// contact("fax", { fax: 1231 });
+function contact<K extends keyof CommunicationMethods>(
+    method: K,
+    contact: CommunicationMethods[K] // 💡turning key into value -- a *mapped type*
+) {
+    //...
+}
+contact("email", { name: "foo", email: "mike@example.com" });
+contact("phone", { name: "foo", phone: 3213332222 });
+contact("fax", { fax: 1231 });
 
-// // we can get all values by mapping through all keys
-// type AllCommKeys = keyof CommunicationMethods;
-// type AllCommValues = CommunicationMethods[keyof CommunicationMethods];
+// we can get all values by mapping through all keys
+type AllCommKeys = keyof CommunicationMethods;
+type AllCommValues = CommunicationMethods[keyof CommunicationMethods];
 
 /**
  * (2) Type queries allow us to obtain the type from a value using typeof
  */
 
-// const alreadyResolvedNum = Promise.resolve(4);
+const alreadyResolvedNum = Promise.resolve(4);
 
-// type ResolveType = typeof Promise.resolve;
+type ResolveType = typeof Promise.resolve;
 
-// const x: ResolveType = Promise.resolve;
-// x(42).then(y => y.toPrecision(2));
+const x: ResolveType = Promise.resolve;
+x(42).then(y => y.toPrecision(2));
 
 /**
  * (3) Conditional types allow for the use of a ternary operator w/ types
  * We can also extract type parameters using the _infer_ keyword
  */
 
-// type EventualType<T> = T extends Promise<infer S> // if T extends Promise<any>
-//   ? S // extract the type the promise resolves to
-//   : T; // otherwise just let T pass through
+// NOTE: we can only use conditional types with generics
+// NOTE: here is the only place we can use the keyword `infer`:
+type EventualType<T> = T extends Promise<infer S> // if T extends Promise<any>
+    ? S // extract the type the promise resolves to
+    : T; // otherwise just let T pass through
 
-// let a: EventualType<Promise<number>>;
-// let b: EventualType<number[]>;
+let a: EventualType<Promise<number>>;
+let b: EventualType<number[]>;
 
 //== Built-in Utility Types ==//
 
